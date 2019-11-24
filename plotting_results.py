96363af74b2dbc2e7eb62a0bfa12f0c6138917da
N8Brooks <natebrooks004@gmail.com>
2019-11-24 12:45:09 -0600

    update
    update

--- plotting_results.py
+++ plotting_results.py
@@ -8,22 +8,45 @@ Created on Fri Nov 22 14:54:36 2019
 import sys
 import pandas as pd
 import numpy as np
-from longest_common_substring import algorithms
+import longest_common_substring as lcs
+
+POWER = 1
 
 # specify or default the file you want to plot
 if len(sys.argv) > 1:
     DATA_TYPE = sys.argv[1]
     assert DATA_TYPE in ['random', 'sided', 'worst', 'text']
 else:
-    DATA_TYPE = 'sided'    # ['random', 'sided', 'worst', 'text']
+    DATA_TYPE = 'text'    # ['random', 'sided', 'worst', 'text']
 
-algorithms = [algo.__name__ for algo in algorithms]
-df = pd.read_csv(f'{DATA_TYPE}_data.csv', index_col='Unnamed: 0')
+# plots single algorithm for each data type
+def plot_algo(name):
+    algorithms = [algo.__name__ for algo in lcs.algorithms]
+    for DATA_TYPE in ['worst', 'random', 'text', 'sided']:
+        df = pd.read_csv(f'{DATA_TYPE}_data.csv', index_col='Unnamed: 0')
+        
+        for algo in algorithms:
+            if algo not in df.columns: continue
+            if algo is not name: continue
+            try:
+                data = df[algo].dropna()
+                params = np.polyfit(data.index, data, POWER)
+                f = np.poly1d(params)
+                df[algo+'_predict'] = pd.Series(data.index.map(f), index=data.index)
+                df[[algo, algo+'_predict']].dropna().plot(title=DATA_TYPE)
+            except:
+                pass
 
-for algo in algorithms:
-    if algo not in df.columns: continue
-    data = df[algo].dropna()
-    params = np.polyfit(data.index, data, 2)
-    f = np.poly1d(params)
-    df[algo+'_predict'] = pd.Series(data.index.map(f), index=data.index)
-    df[[algo, algo+'_predict']].dropna().plot()
+def table_algo(name):
+    algorithms = [algo.__name__ for algo in lcs.algorithms]
+    data = pd.DataFrame()
+    for DATA_TYPE in ['worst', 'random', 'text']:
+        df = pd.read_csv(f'{DATA_TYPE}_data.csv', index_col='Unnamed: 0')
+        if name in df.columns:
+            data[name+'_'+DATA_TYPE] = df[name].dropna()
+            data[name+'_'+DATA_TYPE+'_ratio'] = df[name].pct_change().add(1)
+    
+    return data
+
+if __name__ == '__main__':
+    df = table_algo('suffix_tree_lcs')
--- print_lcs.py
+++ print_lcs.py
@@ -16,7 +16,7 @@ def suffix_search_lcs(a, b):
     len_a, len_b = len(a) + 1, len(b) + 1
     short = min(len(a), len(b))
     
-    tree = SuffixTree(False, [a])
+    tree = SuffixTree(True, [a])
     print('Completed suffix tree')
     
     # returns if there is a common substring of length m between a, b
@@ -47,6 +47,38 @@ def suffix_search_lcs(a, b):
     
     return r
 
+# same as in longest_common_substring.py except with more outpu
+def exp_search_lcs(a, b):
+    # left and right bounds, max sizes
+    len_a, len_b = len(a) + 1, len(b) + 1
+    short = min(len(a), len(b))
+    
+    # returns if there is a common substring of length m between a, b
+    def found_common(m):
+        a_substr = set(a[i-m:i] for i in range(m, len_a))
+        return any(b[i-m:i] in a_substr for i in range(m, len_b))
+    
+    # exponentially increase l and r
+    l, r = 0, 1
+    print(l, r)
+    while r < len_a and found_common(r):
+        l, r = r + 1, r * 2
+        print(l, r)
+    r = min(r, short)
+    print(l, r)
+    
+    # right-most binary search on if substring length is possible
+    while l <= r:
+        m = (l + r) // 2
+        print(m)
+        
+        if found_common(m):
+            l = m + 1
+        else:
+            r = m - 1
+    
+    return r
+
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         print('There needs two be two file name arguments to find the lcs.')
@@ -57,8 +89,8 @@ if __name__ == '__main__':
     b = experiments.read_text(sys.argv[2])
     
     # get the length of lcs
-    length = suffix_search_lcs(a, b)
-    #length = lcs.exp_search_lcs(a, b)
+    #length = suffix_search_lcs(a, b)
+    length = exp_search_lcs(a, b)
     
     # print substrings
-    #lcs.print_common(length, a, b)
+    lcs.print_common(length, a, b)
